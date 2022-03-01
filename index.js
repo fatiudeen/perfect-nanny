19308c778e36e561c76484b35b28c410784f83d3
fatiudeen <shehufatiudeen@gmail.com>
2022-03-01 18:38:57 +0100

    fixed bugs

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-const app = express()
-import morgan from 'morgan'
-import cors from 'cors'
-import db from './config/db.js'
-import {errorHandler} from './middlewares/error.js'
-import _protected from './middlewares/protected.js'
-import adminRoutes from './routes/adminRoute.js'
-import helperRoute from './routes/helperRoute.js'
-import authRoute from './routes/authRoute.js'
-import userRoute from './routes/userRoute.js'
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+import morgan from 'morgan';
+import cors from 'cors';
+import db from './config/db.js';
+import { errorHandler } from './middlewares/error.js';
+import _protected from './middlewares/protected.js';
+import adminRoutes from './routes/adminRoute.js';
+import helperRoute from './routes/helperRoute.js';
+import authRoute from './routes/authRoute.js';
+import userRoute from './routes/userRoute.js';
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type'],
+};
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
 
 //Routes
-app.use('/api/auth', authRoute)
-app.use('/api/admin', _protected('Admin'), adminRoutes)
-app.use('/api/user',_protected('User'), userRoute)
-app.use('/api/helper', _protected('Helper'), helperRoute)
-app.use('*',(req,res)=>{
-  res.status(500).json({
-      status:'Sorry Route does not exists',
-  })
-})
-app.use(errorHandler)
+app.use('/api/auth', authRoute);
+app.use('/api/admin', _protected('Admin'), adminRoutes);
+app.use('/api/user', _protected('User'), userRoute);
+app.use('/api/helper', _protected('Helper'), helperRoute);
+app.use('*', (req, res) => {
+    res.status(500).json({
+        status: 'Sorry Route does not exists',
+    });
+});
+app.use(errorHandler);
 
 //database connection
-db()
+db();
 
 const server = app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-)
+    console.log(`Server running on port ${process.env.PORT}`),
+);
